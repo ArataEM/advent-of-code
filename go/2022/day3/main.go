e27ff861d499a26271d4c15daca6ce4ced7c1fc2
Artem Usenko <arata.em@gmail.com>
2023-07-20 00:14:17 +0300

    day 3 read input

---
+++ go/2022/day3/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	filePath := "input.txt"
+	readFile, err := os.Open(filePath)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	var fileLines []string
+
+	for fileScanner.Scan() {
+		fileLines = append(fileLines, fileScanner.Text())
+	}
+
+	readFile.Close()
+
+}
